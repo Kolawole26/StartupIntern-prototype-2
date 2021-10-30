a2432084de9cd49a7d5781f0667138e20c3ad19d
Kolawole26 <kolenzy26@yahoo.com>
2021-10-30 15:37:55 +0100

    Fourth commit

--- src/MyPages/Register.js
+++ src/MyPages/Register.js
@@ -11,7 +11,7 @@ function Register() {
         setOpenLink(!openLink)
     }
 return (
-    <div className="login">
+    <div className="register">
             <nav className="navbar">
                 <div className="left-side" id={openLink ? "open" : "close"}>
                 <Link to='/'><h2>Eventful Momemts.</h2></Link>
@@ -30,27 +30,29 @@ return (
                 
             </nav>
             <div className="wrapper">
-                    <h1>Create an account</h1>
+                    <div className="layout">
+                        <h1>Create an account</h1>
 
-                    <form>
-                        <div className="form-control">
-                        <label htmlFor="fullname">Fullname</label>
-                        <input type="fullname" name="fullname" />
-                        </div>
-                        
-                        <div className="form-control">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" />
-                        </div>
-                    
-                        <div className="form-control bt-p">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" />
-                        </div>
-                        
-                        <button className="btn">Create</button>
-                        
-                    </form>
+                        <form>
+                            <div className="form-control">
+                            <label htmlFor="fullname">Fullname</label>
+                            <input type="fullname" name="fullname" />
+                            </div>
+                            
+                            <div className="form-control">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" />
+                            </div>
+
+                            <div className="form-control bt-p">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" />
+                            </div>
+                            
+                            <button className="btn">Create</button>
+                            
+                        </form>
+                    </div>
                 </div>
         </div>
     )
